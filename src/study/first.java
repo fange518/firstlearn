28734ae7253f2d603311b8e1b1086146b6b8aac1
fange518 <60872842+fange518@users.noreply.github.com>
2020-11-04 16:00:14 +0800

    Initial commit

---
+++ src/study/first.java
@@ -0,0 +1,49 @@
+package study;
+
+import java.util.Scanner;
+
+public class first {
+    public static void main(String[] args){
+       /* int a=10086;
+        String b="string";
+        float f=120;
+        double d=12;
+        boolean s=false;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(s);*/
+       //数组
+        /*int[] a;
+        a=new int[5];
+        a[]=
+        int[] b=new int[5];
+      System.out.println(a.length);*/
+            /*int[] a = new int[5];
+            a[0] = (int) (Math.random() * 100);
+            a[1] = (int) (Math.random() * 100);
+            a[2] = (int) (Math.random() * 100);
+            a[3] = (int) (Math.random() * 100);
+            a[4] = (int) (Math.random() * 100);
+
+            System.out.println("数组中的各个随机数是:");
+            for (int i = 0; i<a.length; i++)
+                System.out.println(a[i]);
+
+            System.out.println("本练习的目的是，找出最小的一个值: ");*/
+
+            //构建数组
+           /* int[] a=new int[]{1,2,3,4,5,6};
+            System.out.println(a[2]);
+            int[] b={1,2,3,4,5,6};
+            int[] c;*/
+
+
+    }
+}
+
+
+
+
+
---
+++ src/study/superfor.java
@@ -0,0 +1,21 @@
+//2020.11.4
+
+package study;
+
+public class superfor {
+    public static void main(String[] args) {
+
+                int[] values = new int[]{18,62,68,82,65,9};
+                //常规遍历
+                for (int i = 0; i < values.length; i++) {
+                    int each = values[i];
+                    System.out.println(each);
+                }
+
+                //增强型for循环遍历
+                for (int each : values) {
+                    System.out.println(each);
+                }
+
+            }
+        }
